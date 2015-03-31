b0670fc30aa0d767c11cd398b966ea48de0f4e6c
Tom <fanella@purdue.edu>
2015-03-31 11:37:24 -0400

    Make IRCServer

--- HashTableVoid.cc
+++ HashTableVoid.cc
@@ -7,23 +7,48 @@
 // Obtain the hash code of a key
 int HashTableVoid::hash(const char * key)
 {
-  // Add implementation here
-  return 0;
+	int h = 0;
+	const char * p = key;
+	while (*p) {
+		h += *p;
+		p++;
+	}
+	return h % TableSize;
 }
 
 // Constructor for hash table. Initializes hash table
 HashTableVoid::HashTableVoid()
 {
-  // Add implementation here
-       
+	_buckets = (HashTableVoidEntry **) malloc(TableSize *
+				sizeof(HashTableVoidEntry*));
+	for (int i = 0; i < TableSize; i++)
+		_buckets[i] = NULL;
 }
 
 // Add a record to the hash table. Returns true if key already exists.
 // Substitute content if key already exists.
 bool HashTableVoid::insertItem( const char * key, void * data)
 {
-  // Add implementation here
-  return false;
+	// Get hash bucket
+	int h = hash(key);
+
+	HashTableVoidEntry * e = _buckets[h];
+	while (e != NULL) {
+		if (!strcmp(e->_key, key)) {
+			//Entry found
+			e->_data = data;
+			return true;
+		}
+		e = e->_next;
+	}
+
+	//Entry not found. Add it.
+	e = new HashTableVoidEntry;
+	e->_key = strdup(key);
+	e->_data = data;
+	e->_next = _buckets[h];
+	_buckets[h] = e;
+	return false;
 }
 
 // Find a key in the dictionary and place in "data" the corresponding record
