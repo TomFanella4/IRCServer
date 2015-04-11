76bbff1a23e5fb40c390d4d36d7a94e8c2f82ad0
Tom <fanella@purdue.edu>
2015-04-11 15:25:11 -0400

    Make IRCServer

--- IRCServer.cc
+++ IRCServer.cc
@@ -365,7 +365,9 @@ IRCServer::getAllUsers(int fd, const char * user, const char * password,const  c
 		write(fd, msg, strlen(msg));
 		return;
 	}*/
-	
+
+	printf("CU: %d, First: %s", currentUser, users[0].username);
+
 	for (int i = 0; i < currentUser; i++) {
 		const char * uList;
 		uList = users[i].username;
