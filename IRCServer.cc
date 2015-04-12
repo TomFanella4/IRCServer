363be9a2450aee3188c57e6fffaff0b289ae2feb
Tom <fanella@purdue.edu>
2015-04-12 15:40:44 -0400

    Make IRCServer

--- IRCServer.cc
+++ IRCServer.cc
@@ -509,8 +509,8 @@ void IRCServer::sendMessage(int fd, const char * user, const char * password, co
 	
 	// Prints message in messages array
 	int cMessage = rooms[roomNum].currentMessage;
-	sprintf(rooms[roomNum].messages[cMessage], "MSGNUM%d %s %s", cMessage + 1, user, message);
-	printf("MSGNUM%d %s %s", cMessage + 1, user, message);
+	sprintf(rooms[roomNum].messages[cMessage], "MSGNUM%d %s %s\n", cMessage + 1, user, message);
+	printf("MSGNUM%d %s %s\n", cMessage + 1, user, message);
 
 	// Updates server	
 	const char * msg =  "OK\r\n";
